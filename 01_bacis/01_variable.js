c49d96548b1ccd409b1515b48ae7a4f0d40ee8ce
hs9642386 <hs9642386@gmail.com>
2026-08-23 11:17:12 +0000

    container config done

---
+++ 01_bacis/01_variable.js
@@ -0,0 +1,15 @@
+const accountID = 123456
+let accountemail = "Hs9642386@gmail.com"
+var accountCity = "Delhi"
+accountname = "Gauri";
+
+console.log(accountID);
+console.table([accountID,accountemail,accountCity,accountname]);
+
+let accountpassword = "1234567"
+let accoutCity ="noida"
+let state = null
+
+console.log(typeof "Harshita"); // string
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
---
+++ 02_conversionOperation.js
@@ -0,0 +1,8 @@
+let score = 33
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let ValueInNumber= Number(score)
+console.log(typeof ValueInNumber)
+console.log(ValueInNumber)
