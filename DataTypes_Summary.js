b3d8ff67df1a97b72bbbe5e3376d46b66e79b43d
hs9642386 <hs9642386@gmail.com>
2026-09-22 06:24:20 +0000

    commit

---
+++ DataTypes_Summary.js
@@ -0,0 +1,31 @@
+// primitive
+
+//7 types: String, Boolearn, Number , null , Undefined, Symbol, BigInt
+
+const score = 100
+const ScoreValue =100.3
+
+const isLoggedIn =false
+const outsideTemp = null
+let useEmail;
+
+const id = Symbol("123")
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 3457628567n
+
+// Non-primitive
+// Array , Object , Function 
+
+const heros = ["Harshita","priya","Geeta"];
+let myObj = {
+    name: "Harshita",
+    age: 22,
+}
+const myFunctrion = function(){
+    console.log("Hello world");
+}
+
+console.log(typzf bigNumber);
